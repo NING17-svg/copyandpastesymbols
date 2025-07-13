b5b1d5c9198cd2f89561efbdbec75a7b2ec271ed
unknown <445930143@qq.com>
2025-07-13 22:12:08 +0800

    添加站点地图和机器人

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,11 @@ export const metadata: Metadata = {
   title: "Cool Symbols to Copy and Paste | Text Symbols | copyandpastesymbols.pro",
   description: "Find and copy cool symbols, emoji, text decorations and more. Use our collection of special characters for social media, messages, and profiles.",
   keywords: "symbols, copy and paste, cool symbols, text decoration, emoji, special characters, unicode symbols",
+  icons: {
+    icon: [
+      { url: '/favicon.ico', type: 'image/x-icon' },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -29,6 +34,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="canonical" href="https://copyandpastesymbols.pro/" />
+        <meta name="robots" content="noindex" data-path="/favicon.ico" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
